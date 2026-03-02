6a13a6f197f3c8878441022416bd441d81324d6c
Jeff Walden <jwalden@mit.edu>
2026-03-02 00:24:34 -0800

    refactor: Perform strict/loose manifest validation using primarily JSON schemas that modules can use in their own manifests.  (#198)

--- packages/base/tools/schema-compile.mjs
+++ packages/base/tools/schema-compile.mjs
@@ -6,14 +6,21 @@ import standaloneCode from 'ajv/dist/standalone/index.js'
 import { $ } from 'zx'
 import looseManifestSchema from '../assets/loose-manifest.schema.json' with { type: 'json' }
 import manifestSchema from '../assets/manifest.schema.json' with { type: 'json' }
+import strictRefinementsSchema from '../assets/strict-refinements.schema.json' with { type: 'json' }
 
 // Compile Typescript definitions from the JSON schema
 await $`json2ts --cwd assets --input assets/manifest.schema.json --output generated/manifest.d.ts --additionalProperties=false`
 
 {
 	// The generated code be in esm export format:
-	const ajv = new Ajv2020({ schemas: [manifestSchema, looseManifestSchema], code: { source: true, esm: true } })
-	const moduleCode = standaloneCode(ajv, { ModuleManifest: '/assets/manifest.schema.json' })
+	const ajv = new Ajv2020({
+		schemas: [looseManifestSchema, manifestSchema, strictRefinementsSchema],
+		code: { source: true, esm: true },
+	})
+	const moduleCode = standaloneCode(ajv, {
+		LooseModuleManifest: '/assets/loose-manifest.schema.json',
+		ModuleManifest: '/assets/manifest.schema.json',
+	})
 
 	// Now you can write the module code to file
 	const outputPath = new URL('../generated/validate_manifest.js', import.meta.url)
