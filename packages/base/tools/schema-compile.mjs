a60205e4e7d7f50eced1b4515ee77779626da5a9
Jeff Walden <jwalden@mit.edu>
2026-03-01 21:52:08 -0800

    refactor: Rename `assets/manifest.schema.json` to `assets/loose-manifest.schema.json`.

--- packages/base/tools/schema-compile.mjs
+++ packages/base/tools/schema-compile.mjs
@@ -4,10 +4,10 @@ import { fileURLToPath } from 'url'
 import Ajv2020 from 'ajv/dist/2020.js'
 import standaloneCode from 'ajv/dist/standalone/index.js'
 import { $ } from 'zx'
-import manifestSchema from '../assets/manifest.schema.json' with { type: 'json' }
+import manifestSchema from '../assets/loose-manifest.schema.json' with { type: 'json' }
 
 // Compile Typescript definitions from the JSON schema
-await $`json2ts --input assets/manifest.schema.json --output generated/manifest.d.ts --additionalProperties=false`
+await $`json2ts --input assets/loose-manifest.schema.json --output generated/manifest.d.ts --additionalProperties=false`
 
 {
 	// The generated code be in esm export format:
